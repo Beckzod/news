1c4bc868d302008a6a3fc5dacd90d0e6d0b58b85
Beckzod <jumabaevbeckzod@gmail.com>
2023-11-25 20:32:28 +0500

    models create

--- news_app/models.py
+++ news_app/models.py
@@ -1,3 +1,18 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    class Status(models.TextChoices):
+        Draft = "DF", "Draft"
+        Published = "PB", "Published"
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    image = models.ImageField(upload_to='news/images')
+    category = models.ForeignKey()
+    publish_time = models.DateTimeField(default=timezone.now)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices,
+                              default=Status.Draft)
