5be915c8491ad3da4dfb224037f74872cfc349fd
Beckzod <jumabaevbeckzod@gmail.com>
2023-11-28 23:04:14 +0500

    update files

--- news_app/models.py
+++ news_app/models.py
@@ -2,6 +2,18 @@ from django.utils import timezone
 from django.db import models
 
 
+class PubManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status="Published")
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
 class News(models.Model):
     class Status(models.TextChoices):
         Draft = "DF", "Draft"
@@ -10,9 +22,17 @@ class News(models.Model):
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     image = models.ImageField(upload_to='news/images')
-    category = models.ForeignKey()
-    publish_time = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    published_time = models.DateTimeField(default=timezone.now)
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices,
                               default=Status.Draft)
+    objects = models.Manager()
+    published = PubManager()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-published_time']
